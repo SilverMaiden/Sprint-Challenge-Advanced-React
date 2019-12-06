e4bac8022dc1f309de72b065f11ec113e465a5c6
SilverMaiden <h.specials@gmail.com>
2019-12-06 08:55:46 -0800

    installed node-sass for styles.scss

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 
 export const useLocalStorage = (key, initialValue) => {
-    const [storedValue, setStoredValue] useState(() => {
+    const [storedValue, setStoredValue] = useState(() => {
         if (window.localStorage.getItem(key)) {
             return JSON.parse(window.localStorage.getItem(key));
         }
